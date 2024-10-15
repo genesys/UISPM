4417738eca7330380d3f90e70648fa04283a7805
Golan Shoval Gil <golan.shoval_gil@genesys.com>
2024-10-15 17:46:15 +0300

    Updating GenesysCloud setup

--- UISPM/Package.swift
+++ UISPM/Package.swift
@@ -1,21 +1,36 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.4
 
 import PackageDescription
 
 let package = Package(
-    name: "UISPM",
+    name: "GenesysCloud",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "UISPM",
-            targets: ["UISPM"]),
+            name: "GenesysCloud",
+            targets: ["GenesysCloudWrapper"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/GolanSG/MessengerSPM.git",
+            from: "1.10.1"
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "GenesysCloud",
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloud/GenesysCloud_version_t1.10.1_commit_755e8ab3dc1e53022111d4218059d689966fb237.zip",
+            checksum: "d9eaa21bece26207b303787b448ac0b2251cb43980f7775aa6dfdec033926663"
+        ),
         .target(
-            name: "UISPM"),
-
+            name: "GenesysCloudWrapper",
+            dependencies: [
+                "GenesysCloud",
+                .product(name: "GenesysCloudMessenger", package: "MessengerSPM")
+            ]
+        )
     ]
 )
---
+++ UISPM/Sources/GenesysCloudWrapper/GenesysCloudWrapper.swift
@@ -0,0 +1,17 @@
+//
+//  GenesysCloudWrapper.swift
+//  GenesysCloud
+//
+//  Created by Golan Shoval Gil on 15/10/2024.
+//
+
+// Sources/GenesysCloudWrapper/GenesysCloudWrapper.swift
+
+import GenesysCloudCore
+import GenesysCloudMessenger
+import GenesysCloud
+import MessengerTransport
+
+public class GenesysCloudWrapper {
+    // This is a placeholder class. Implement your wrapper logic here.
+}
