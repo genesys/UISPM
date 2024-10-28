368124aae2bd81cf21b45f8816616981fa4a8943
Golan Shoval Gil <golan.shoval_gil@genesys.com>
2024-10-28 13:59:43 +0200

    Bumped version to 1.10.2 again

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloud",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloud/GenesysCloud_version_t1.10.1_commit_755e8ab3dc1e53022111d4218059d689966fb237.zip",
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloud/GenesysCloud_version_t1.10.2_commit_2ac9bca36c9079823357da70ef5d0b4047a5c157.zip",
             checksum: "8133b8f1b1063fa85fa2258bc47809fa6ff000d4bad33115b403a059bb34e6f2"
         ),
         .target(
